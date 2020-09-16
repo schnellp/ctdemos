221f67e511ad0efdaf6ce72d989867f2e0794ce0
Patrick Schnell <schnell.31@osu.edu>
2020-09-16 13:40:20 -0400

    Changed data generation for analysis demo

--- analysis/R/data-generation.R
+++ analysis/R/data-generation.R
@@ -5,21 +5,28 @@ generate.data.2 <- function() {
   N <- n.per.arm * 2
   
   assignment <- rep(c("Treatment", "Control"), times = n.per.arm)[sample(N)]
-  exposure <- assignment
-  exposure[exposure == "Treatment"][as.logical(rbinom(n.per.arm, 1, 0.2))] <- "Control"
   assignment <- factor(assignment, levels = c("Control", "Treatment"))
-  exposure <- factor(exposure, levels = c("Control", "Treatment"))
   
   outcome.binary <- rbinom(N, 1,
-                           prob = 0.5 + 0.3 * (assignment == "treatment"))
+                           prob = 0.5 + 0.3 * (assignment == "Treatment"))
   outcome.binary <- as.factor(ifelse(outcome.binary == 1, "Yes", "No"))
+
+  exposure <- assignment
+  assigned <- assignment == "Treatment"
+  exposure[assigned][
+    as.logical(rbinom(n.per.arm, 1,0.1 + 0.3 * (outcome.binary[assigned] == "Yes")))
+    ] <- "Control"
+  exposure <- factor(exposure, levels = c("Control", "Treatment"))
+  
   
   outcome.continuous <- rnorm(N,
-                              mean = -0.2 * (assignment == "treatment") -
-                                0.5 * (exposure == "treatment"))
+                              mean = 1 * (assignment == "Treatment") +
+                                1 * (exposure == "Treatment"))
   
   outcome.tte.event <- rgamma(N, shape = 3,
-                              rate = (1 + 1 * (exposure == "treatment")) / 10)
+                              rate = ifelse(exposure == "Treatment", 1 / 40,
+                                            ifelse(assignment == "Treatment", 1 / 1,
+                                                   1 / 30)))
   outcome.tte.censor <- 60
   outcome.tte.observation <- pmin(outcome.tte.event, outcome.tte.censor)
   
