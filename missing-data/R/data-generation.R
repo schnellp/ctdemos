02427ab46d0400931770ff4de7c3bbc529c1cf78
Patrick Schnell <schnell.31@osu.edu>
2020-10-15 16:42:11 -0400

    Emphasize difference between methods
    Tweaks data generation so that imputation is much more likely to yield a different result than missing outcome deletion.

--- missing-data/R/data-generation.R
+++ missing-data/R/data-generation.R
@@ -1,7 +1,7 @@
 generate.data <- function() {
   set.seed(0)
   
-  n.per.arm <- 20
+  n.per.arm <- 30
   N <- n.per.arm * 2
   
   regimen <- rep(c("paclitaxel", "docetaxel"), times = n.per.arm)[sample(N)]
@@ -10,20 +10,26 @@ generate.data <- function() {
   
   bmi <- round(rbeta(N, 5, 5) * (40 - 15) + 15)
   
-  theta <- rnorm(N, mean = ifelse(regimen == "paclitaxel", 2, 3.2) + age / 100, sd = sqrt(10))
-  
-  follow.up <- pmax(1, pmin(round(rexp(N, rate = ifelse(regimen == "paclitaxel", 1 / 50, 1 / 20))), 5))
-  
   y <- matrix(NA, nrow = N, ncol = 5)
   y.obs <- y
   y[, 1] <- rnorm(N, mean = (age / 10) ^ 2)
   y.obs[, 1] <- pmax(0, y[, 1])
+  
+  theta <- rnorm(N,
+                 mean = ifelse(regimen == "paclitaxel",
+                               2,
+                               -1 + ifelse(age >= 80, 10, 2)),
+                 sd = sqrt(10))
+  
+  follow.up <- pmax(1, pmin(round(rexp(N, rate = ifelse(age >= 80, 1 / 10, 1 / 100))), 5))
+  
   for (j in 2 : 5) {
     y[, j] <- y[, j - 1] + theta + rnorm(N)
-    available <- as.logical(rbinom(N, 1, prob = pmax(0.7, age / 90)))
+    available <- (j == 5) | as.logical(rbinom(N, 1, prob = 0.8))
     y.obs[available, j] <- pmax(0, y[available, j])
     y.obs[follow.up < j, j] <- NA
   }
+  
   
   data <- data.frame(subject = as.factor(1 : N),
                      age = age,
@@ -34,9 +40,6 @@ generate.data <- function() {
                      cycle.2 = round(y.obs[, 3]),
                      cycle.3 = round(y.obs[, 4]),
                      follow.up = round(y.obs[, 5]))
-  
-  # t.test((y.obs[, 5] - y.obs[, 1]) ~ regimen, var.equal = FALSE)
-  # t.test((y[, 5] - y[, 1]) ~ regimen, var.equal = FALSE)
   
   data
 }
